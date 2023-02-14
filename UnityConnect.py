33f0c045d7f80cd97d33e3f37d5bcdc6ae85c69e
Seohyun Yang <seoh136199@gmail.com>
2023-02-14 18:27:19 +0900

    서버스터디 5주차 과제

--- UnityConnect.py
+++ UnityConnect.py
@@ -1,10 +1,26 @@
 from flask import Flask, request
+from database import Database
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def connect():
-    return "연결 성공"
+@app.route('/', methods=['GET'])
+def get(self):
+    id = request.args.get("id")
+    pw = request.args.get("password")
+
+    db = Database()
+    sql = "SELECT * FROM user where id = %s"
+    result = db.execute_one(sql, (id))
+
+    db.commit()
+    db.close()
+
+    if (result == None):
+        return False, 400
+    elif (result['password'] != pw):
+        return False, 400
+    else:
+        return True, 200
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
