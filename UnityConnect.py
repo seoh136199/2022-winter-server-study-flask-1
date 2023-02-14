e4a129f5c3c7893785228660e77bbf067ac245cb
Seohyun Yang <seoh136199@gmail.com>
2023-02-14 20:58:06 +0900

    d

--- UnityConnect.py
+++ UnityConnect.py
@@ -16,11 +16,11 @@ def get():
     db.close()
 
     if (result == None):
-        return False, 400
+        return "False", 400
     elif (result['password'] != pw):
-        return False, 400
+        return "False", 400
     else:
-        return True, 200
+        return "True", 200
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
